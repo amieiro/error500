cca1a50fcc1484e161335d0fa908c33db6b04d7a
Jesus Amieiro <amieiro@gmail.com>
2014-11-05 22:38:53 +0100

    Add the date to each post in the main loop and in the individual post

--- functions.php
+++ functions.php
@@ -9,3 +9,169 @@ add_action( 'after_setup_theme', 'error500_setup' );
 function error500_setup() {
 	load_child_theme_textdomain( 'error500', get_stylesheet_directory() . '/languages' );
 }
+
+/**
+ * Prints HTML with meta information for current post: date
+ *
+ * @since error500 2.1.0
+ *
+ * @return void
+ */
+if ( ! function_exists( 'error500_posted_before' ) ) {
+	function error500_posted_before() {
+		$post_icon = '';
+		switch ( get_post_format() ) {
+			case 'aside':
+				$post_icon = 'fa-file-o';
+				break;
+			case 'audio':
+				$post_icon = 'fa-volume-up';
+				break;
+			case 'chat':
+				$post_icon = 'fa-comment';
+				break;
+			case 'gallery':
+				$post_icon = 'fa-camera';
+				break;
+			case 'image':
+				$post_icon = 'fa-picture-o';
+				break;
+			case 'link':
+				$post_icon = 'fa-link';
+				break;
+			case 'quote':
+				$post_icon = 'fa-quote-left';
+				break;
+			case 'status':
+				$post_icon = 'fa-user';
+				break;
+			case 'video':
+				$post_icon = 'fa-video-camera';
+				break;
+			default:
+				$post_icon = 'fa-calendar';
+				break;
+		}
+
+		// Translators: 1: Icon 2: Permalink 3: Post date and time 4: Publish date in ISO format 5: Post date
+		$date = sprintf( '<i class="fa %1$s"></i> <a href="%2$s" title="Posted %3$s" rel="bookmark"><time class="entry-date" datetime="%4$s" itemprop="datePublished">%5$s</time></a>',
+			$post_icon,
+			esc_url( get_permalink() ),
+			sprintf( esc_html__( '%1$s @ %2$s', 'quark' ), esc_html( get_the_date() ), esc_attr( get_the_time() ) ),
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() )
+		);
+
+		// Translators: 1: Date link 2: Author link 3: Categories 4: No. of Comments
+		$author = ""; /*sprintf( '<i class="fa fa-pencil"></i> <address class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></address>',
+			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+			esc_attr( sprintf( esc_html__( 'View all posts by %s', 'quark' ), get_the_author() ) ),
+			get_the_author()
+		);*/
+
+		// Return the Categories as a list
+		$categories_list = ""; //get_the_category_list( esc_html__( ' ', 'quark' ) );
+
+		// Translators: 1: Permalink 2: Title 3: No. of Comments
+		$comments = ""; /*sprintf( '<span class="comments-link"><i class="fa fa-comment"></i> <a href="%1$s" title="%2$s">%3$s</a></span>',
+			esc_url( get_comments_link() ),
+			esc_attr( esc_html__( 'Comment on ' . the_title_attribute( 'echo=0' ) ) ),
+			( get_comments_number() > 0 ? sprintf( _n( '%1$s Comment', '%1$s Comments', get_comments_number(), 'quark' ), get_comments_number() ) : esc_html__( 'No Comments', 'quark' ) )
+		);*/
+
+		// Translators: 1: Date 2: Author 3: Categories 4: Comments
+		printf( wp_kses( __( '<div class="header-meta">%1$s%2$s<span class="post-categories">%3$s</span>%4$s</div>', 'quark' ), array( 
+			'div' => array (
+				'class' => array() ),
+			'span' => array(
+				'class' => array() ) ) ),
+			$date,
+			$author,
+			$categories_list,
+			( is_search() ? '' : $comments )
+		);
+	}
+}
+
+/**
+ * Prints HTML with meta information for current post: comments
+ *
+ * @since error500 2.1.0
+ *
+ * @return void
+ */
+if ( ! function_exists( 'error500_posted_after' ) ) {
+	function error500_posted_after() {
+		$post_icon = '';
+		switch ( get_post_format() ) {
+			case 'aside':
+				$post_icon = 'fa-file-o';
+				break;
+			case 'audio':
+				$post_icon = 'fa-volume-up';
+				break;
+			case 'chat':
+				$post_icon = 'fa-comment';
+				break;
+			case 'gallery':
+				$post_icon = 'fa-camera';
+				break;
+			case 'image':
+				$post_icon = 'fa-picture-o';
+				break;
+			case 'link':
+				$post_icon = 'fa-link';
+				break;
+			case 'quote':
+				$post_icon = 'fa-quote-left';
+				break;
+			case 'status':
+				$post_icon = 'fa-user';
+				break;
+			case 'video':
+				$post_icon = 'fa-video-camera';
+				break;
+			default:
+				$post_icon = 'fa-calendar';
+				break;
+		}
+
+		// Translators: 1: Icon 2: Permalink 3: Post date and time 4: Publish date in ISO format 5: Post date
+		$date = ""; /*sprintf( '<i class="fa %1$s"></i> <a href="%2$s" title="Posted %3$s" rel="bookmark"><time class="entry-date" datetime="%4$s" itemprop="datePublished">%5$s</time></a>',
+			$post_icon,
+			esc_url( get_permalink() ),
+			sprintf( esc_html__( '%1$s @ %2$s', 'quark' ), esc_html( get_the_date() ), esc_attr( get_the_time() ) ),
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() )
+		);*/
+
+		// Translators: 1: Date link 2: Author link 3: Categories 4: No. of Comments
+		$author = ""; /*sprintf( '<i class="fa fa-pencil"></i> <address class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></address>',
+			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+			esc_attr( sprintf( esc_html__( 'View all posts by %s', 'quark' ), get_the_author() ) ),
+			get_the_author()
+		);*/
+
+		// Return the Categories as a list
+		//$categories_list = get_the_category_list( esc_html__( ' ', 'quark' ) );
+
+		// Translators: 1: Permalink 2: Title 3: No. of Comments
+		$comments = sprintf( '<span class="comments-link"><i class="fa fa-comment"></i> <a href="%1$s" title="%2$s">%3$s</a></span>',
+			esc_url( get_comments_link() ),
+			esc_attr( esc_html__( 'Comment on ' . the_title_attribute( 'echo=0' ) ) ),
+			( get_comments_number() > 0 ? sprintf( _n( '%1$s Comment', '%1$s Comments', get_comments_number(), 'quark' ), get_comments_number() ) : esc_html__( 'No Comments', 'quark' ) )
+		);
+
+		// Translators: 1: Date 2: Author 3: Categories 4: Comments
+		printf( wp_kses( __( '<div class="header-meta">%1$s%2$s<span class="post-categories">%3$s</span>%4$s</div>', 'quark' ), array( 
+			'div' => array (
+				'class' => array() ),
+			'span' => array(
+				'class' => array() ) ) ),
+			$date,
+			$author,
+			$categories_list,
+			( is_search() ? '' : $comments )
+		);
+	}
+}
